1c9354ed1e673ae8a0675e37dac854dd98d1697a
sneesna <snaasnee@gmail.com>
2024-11-19 19:27:49 -0500

    Update gui.py
    Changed the main function to have the correct GUI. The buttons don't do anything yet

--- acousticModeling/gui.py
+++ acousticModeling/gui.py
@@ -130,90 +130,75 @@ def alert(msg):
 if __name__ == "__main__": # execute logic if run directly
     _root = Tk() # instantiate instance of Tk class
     _root.title('Acoustic Modeling')
+
+    # THIS IS THE TOP BAR OF CLOSE AND MAXIMIZE/MINIMIZE, AND THE TITLE
     _mainframe = ttk.Frame(_root, padding='5 5 5 5 ') # root is parent of frame
     _mainframe.grid(row=0, column=0, sticky=("E", "W", "N", "S")) # placed on first row,col of parent
     # frame can extend itself in all cardinal directions
-    _url_frame = ttk.LabelFrame(
-        _mainframe, text='URL', padding='5 5 5 5')  # label frame
-    _url_frame.grid(row=0, column=0, sticky=("E", "W"))  # only expands E W
-    _url_frame.columnconfigure(0, weight=1)
-    _url_frame.rowconfigure(0, weight=1)  # behaves when resizing
 
-    _url = StringVar()
-    _url.set('http://localhost:8000')  # sets initial value of _url
-    _url_entry = ttk.Entry(
-        _url_frame, width=40, textvariable=_url)  # text box
-    _url_entry.grid(row=0, column=0, sticky=(E, W, S, N), padx=5)
+    # THIS CREATES A WINDOW THAT FITS TO WHATEVER IS ON IT
+    _window_frame = ttk.LabelFrame(
+        _mainframe, text='Select file:', padding='5 5 5 5')  # label frame
+    _window_frame.grid(row=0, column=0, sticky=("E", "W"))  # only expands E W
+    _window_frame.columnconfigure(0, weight=1)
+    _window_frame.rowconfigure(0, weight=1)  # behaves when resizing
+
+    ####### IMPORT DATA MAIN FUNCTION (CALL COMMAND)
+    # NO FUNCTIONALITY YET
+    # CREATES SMALL TEXT BOX
+    _window = StringVar()
+    _window.set('')  # sets initial value of _window (it is blank)
+    _window_entry = ttk.Entry(
+        _window_frame, width=40, textvariable=_window)  # text box
+    _window_entry.grid(row=0, column=0, sticky=(E, W, S, N), padx=5)
+
+    # BUTTON TO THE RIGHT OF THE TEXT BOX TO INITIATE A COMMAND THAT FINDS THE FILE IN THE TEXT BOX
+    # FILE MUST BE IN THE SAME DIRECTORY
     # grid mgr places object at position
     _fetch_info_btn = ttk.Button(
-        _url_frame, text='Fetch info', command=fetch_url)  # create button
-    # fetch_url() is callback for button press
+        _window_frame, text='Get file', )  # create button, MAKE A COMMAND LATER FOR FINDING THE FILE
+    # fetch_window() is callback for button press
     _fetch_info_btn.grid(row=0, column=1, sticky=W, padx=2)
+    ####### END IMPORT DATA MAIN FUNCTION
 
-#What I ADDED START
-    _fetch_title_btn = ttk.Button(
-        _url_frame, text='Fetch title', command=fetch_title)
-    _fetch_title_btn.grid(row=1, column=1, sticky=W, padx=2)
+    ####### CLEAN DATA IS INTERNAL FUNCTIONS, NOT GUI, BUT STILL DO IT LATER
 
-    # Create button for fetching links
-    _fetch_link_btn = ttk.Button(
-        _url_frame, text='Fetch link', command=fetch_links)
-    _fetch_link_btn.grid(row=2, column=1, sticky=W, padx=2)
-#WHAT I ADDED END
+    # NEW WINDOW SECTION
+    # analysis_frame contains Listbox and Radio Frame
+    _analysis_frame = ttk.LabelFrame(
+        _mainframe, text='Summary', padding='9 0 0 0')
+    _analysis_frame.grid(row=3, column=0, sticky=(N, S, E, W))
 
-    # img_frame contains Lisbox and Radio Frame
-    _img_frame = ttk.LabelFrame(
-        _mainframe, text='Content', padding='9 0 0 0')
-    _img_frame.grid(row=3, column=0, sticky=(N, S, E, W))
+    ####### DATA ANALYSIS SECTION
+    # Set _analysis_frame as parent of Listbox and _summary is variable tied to
+    # IN THIS, "GENERATE SUMMARY STATISTICS AND DESCRIPTIVE MEASURES SUCH AS LENGTH OF AUDIO
+    # SAMPLE AND RT60 VALUE"
+    # Basically, put the summary statistics like length of audio and rt60 into this box
+    # Show graphs etc as you fill this in a function
+    _summary = StringVar()
+    _analysis_listbox = Listbox(
+        _analysis_frame, listvariable=_summary, height=6, width=50)
+    _analysis_listbox.grid(row=0, column=0, sticky=(E, W), pady=5)
 
-    # Set _img_frame as parent of Listbox and _images is variable tied to
-    _images = StringVar()
-    _img_listbox = Listbox(
-        _img_frame, listvariable=_images, height=6, width=25)
-    _img_listbox.grid(row=0, column=0, sticky=(E, W), pady=5)
-    #Scrollbar can move vertical
+
+    # SCROLLBAR, DELETE at the end if it's redundant
     _scrollbar = ttk.Scrollbar(
-        _img_frame, orient=VERTICAL, command=_img_listbox.yview)
+        _analysis_frame, orient=VERTICAL, command=_analysis_listbox.yview)
     _scrollbar.grid(row=0, column=1, sticky=(S, N), pady=6)
-    _img_listbox.configure(yscrollcommand=_scrollbar.set)
-
-    #Listbox occupies (0,0) on _img_frame.
+    _analysis_listbox.configure(yscrollcommand=_scrollbar.set)
+    '''
+    #Listbox occupies (0,0) on _analysis_frame.
     # Scrollbar occupies (0,1) so _radio_frame goes to (0,2)
-    _radio_frame = ttk.Frame(_img_frame)
-    _radio_frame.grid(row=0, column=2, sticky=(N, S, W, E))
-
-    # place label and padding
-    # radio buttons are children of _radio_frame
-    _choice_lbl = ttk.Label(
-        _radio_frame, text="Choose how to save images")
-    _choice_lbl.grid(row=0, column=0, padx=5, pady=5)
-    _save_method = StringVar()
-    _save_method.set('img')
-    # Radiobutton connected to _save_method variable
-    # Know which button is selected by checking value
-    _img_only_radio = ttk.Radiobutton(
-        _radio_frame, text='As Images', variable=_save_method,
-        value='img')
-    _img_only_radio.grid(row=1, column=0,padx=5, pady=2, sticky="W")
-    _img_only_radio.configure(state='normal')
-    _json_radio = ttk.Radiobutton(
-        _radio_frame, text='As JSON', variable=_save_method,
-        value='json')
-    _json_radio.grid(row=2, column=0, padx=5, pady=2, sticky="W")
-
-    # save command saves images to be listed in Listbox after parsing
-    _scrape_btn = ttk.Button(
-        _mainframe, text='Scrape!', command=save)
-    _scrape_btn.grid(row=4, column=0, sticky=E, pady=5)
+    _radio_frame = ttk.Frame(_analysis_frame)
+    _radio_frame.grid(row=0, column=2, sticky=(N, S, W, E))'''
 
     _status_frame = ttk.Frame(
         _root, relief='sunken', padding='2 2 2 2')
     _status_frame.grid(row=1, column=0, sticky=("E", "W", "S"))
     _status_msg = StringVar() # need modified when update status text
-    _status_msg.set('Type a URL to start scraping...')
+    _status_msg.set('Insert a .wav file to start analyzing...')
     _status= ttk.Label(
         _status_frame, textvariable=_status_msg, anchor=W)
     _status.grid(row=0, column=0, sticky=(E, W))
 
-    _root.mainloop() # listens for events, blocks any code that comes after it
-
+_root.mainloop() # listens for events, blocks any code that comes after it
